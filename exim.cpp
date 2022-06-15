c03d5d26cbb2d2b9bdee9e69fb3976b68f3f22a5
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-15 22:30:02 +0530

    learn about useing terminal for taking and showing data

---
+++ exim.cpp
@@ -0,0 +1,30 @@
+// learning about import and export
+// std:: cout, cin, cerr, clog
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cerr << "you are caught " << std::endl;
+    std::clog << "your logs" << std::endl;
+    std::cout << "enter your name" << std::endl;
+    // make a variable for name
+    std::string name;
+
+    std::cout << "Enter you age" << std::endl;
+    int age;
+    std::cin >> name >> age;
+    std::cout << "Hello " << name << "you are  " << age << std::endl;
+    // get data with spaces
+    std::cout << "enter your name and age" << std::endl;
+    std::cin >> name >> age;
+    std::cout << "Hello " << name << "you are  " << age << std::endl;
+    // get data with spaces
+    std::string full_name;
+    std::cout << "enter your full name" << std::endl;
+    std::cin >> full_name;
+    std::cout << "Hello " << full_name << std::endl;
+
+    return 0;
+}
--- functions.cpp
+++ functions.cpp
@@ -3,13 +3,18 @@ int add(int a, int b)
 {
     return a + b;
 }
+int multiply(int a, int b)
+{
+    return a * b;
+}
 
 int main(int argc, char const *argv[])
 {
 
     std::cout << "Hello, World!" << std::endl;
-    int first {1};
-    int second {2};
+    int first{1};
+    int second{2};
     std::cout << "the sum is for 12, 12 " << add(12, 12) << std::endl;
+    std::cout << "the multiplication of 112, 12 id " << multiply(112, 12) << std::endl;
     return 0;
 }
