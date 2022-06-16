e85b62eb0e0f1e6010c42603f3dba9ca4634745e
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-16 18:24:07 +0530

    added auto key in cpp

--- char.cpp
+++ char.cpp
@@ -2,6 +2,9 @@
 int main(int argc, char const *argv[])
 {
     char value = 65;
+    char value2 = 'A';
+    auto value3 = 'A';
     std::cout << value << std::endl;
+    std::cout << value2 << value3 << std::endl;
     return 0;
 }
