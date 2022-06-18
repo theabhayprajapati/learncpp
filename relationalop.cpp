5f052db91059c4e233c89005386b3e1dd11794bc
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-18 08:57:29 +0530

    1/0 true/false

--- relationalop.cpp
+++ relationalop.cpp
@@ -6,6 +6,18 @@ int main(int argc, char const *argv[])
     // compare
     std::cout << "Compare: " << std::endl;
     std::cout << std::boolalpha << (value1 == value2) << std::endl;
+    std::cout << (value1 < value2) << std::endl;
+    std::cout << (value1 > value2) << std::endl;
+    std::cout << (value1 <= value2) << std::endl;
+    std::cout << std::boolalpha << (value1 >= value2) << std::endl;
+    // do comparing in form of 1/0
+    std::cout << "Compare in form of 1/0: " << std::endl;
+    std::cout << (value1 == value2) << std::endl;
+    std::cout << (value1 < value2) << std::endl;
+    std::cout << (value1 > value2) << std::endl;
+    std::cout << (value1 <= value2) << std::endl;
+    std::cout << (value1 >= value2) << std::endl;
+
     // addition
     // print trure false for the result
     return 0;
