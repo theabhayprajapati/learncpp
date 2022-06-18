3059efa831f6df1d6b2e39625fe22e53dda13bdd
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-18 08:40:03 +0530

    added compound form addition

---
+++ compound.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    int value{5};
+    std::cout << value << std::endl;
+    // Addition in Compoung form
+    value +=5;
+    std::cout << value << std::endl;
+    // Subtraction in Compoung form
+    value -=5;
+    std::cout << value << std::endl;
+    // Multiplication in Compoung form
+    value *=5;
+    std::cout << value << std::endl;
+    // Division in Compoung form
+    value /=5;
+    std::cout << value << std::endl;
+    // for using compoung form 
+    // just use the operator +, -, *, /
+    // with = sign
+    // for example:
+    // +=, -=, *=, /=
+    
+    return 0;
+}
