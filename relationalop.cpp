b816806c264eac8f0cdc9fc17aefbccbb69791f2
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-18 08:50:48 +0530

    make a comparision to the values with ==, != and more also use bool alpha to compare in form of true/false it not 1/0

---
+++ relationalop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    int value1 = 5;
+    int value2 = 5;
+    // compare
+    std::cout << "Compare: " << std::endl;
+    std::cout << std::boolalpha << (value1 == value2) << std::endl;
+    // addition
+    // print trure false for the result
+    return 0;
+}
